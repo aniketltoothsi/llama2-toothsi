96713342c07ff2d4a8e03f2aba3ac012515a7f67
aniketltoothsi <97279554+aniketltoothsi@users.noreply.github.com>
2023-07-31 22:41:37 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,8 +8,8 @@ from langchain.chains import RetrievalQA
 import chainlit as cl
 from flask_cors import CORS
 DB_FAISS_PATH = 'vectorstore/db_faiss'
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 custom_prompt_template = """Use the following pieces of information to answer the user's question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
@@ -66,55 +66,32 @@ def final_result(query):
     response = qa_result({'query': query})
     return response
 
-# #chainlit code
-# @cl.on_chat_start
-# async def start():
-#     chain = qa_bot()
-#     msg = cl.Message(content="Starting the bot...")
-#     await msg.send()
-#     msg.content = "Hi, Welcome to Toothsi Bot. What is your query?"
-#     await msg.update()
-
-#     cl.user_session.set("chain", chain)
-
-# @cl.on_message
-# async def main(message):
-#     chain = cl.user_session.get("chain") 
-#     cb = cl.AsyncLangchainCallbackHandler(
-#         stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
-#     )
-#     cb.answer_reached = True
-#     res = await chain.acall(message, callbacks=[cb])
-#     answer = res["result"]
-#     sources = res["source_documents"]
-
-#     if sources:
-#         answer += f"\nSources:" + str(sources)
-#     else:
-#         answer += "\nNo sources found"
-
-#     await cl.Message(content=answer).send()
-
-@app.route('/', methods=['GET'])
-def ping():
-    return 'Pong'
-
-@app.route('/result', methods=['POST'])
-async def detect_teeth():
-    query = request.json.get('query')
-
+#chainlit code
+@cl.on_chat_start
+async def start():
     chain = qa_bot()
-    
-    res = await chain.acall(query)
-    answer = res["result"]    
-    print("aniket",res, "nilesh", answer)
+    msg = cl.Message(content="Starting the bot...")
+    await msg.send()
+    msg.content = "Hi, Welcome to Toothsi Bot. What is your query?"
+    await msg.update()
 
-    # Extract the actual response from the generated response
-    # answer = response['answers'][0]['answer'] if 'answers' in response else "No answer found"
+    cl.user_session.set("chain", chain)
 
-    return jsonify({"answer": answer})      
+@cl.on_message
+async def main(message):
+    chain = cl.user_session.get("chain") 
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    res = await chain.acall(message, callbacks=[cb])
+    answer = res["result"]
+    sources = res["source_documents"]
 
+    if sources:
+        answer += f"\nSources:" + str(sources)
+    else:
+        answer += "\nNo sources found"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    await cl.Message(content=answer).send()
 
